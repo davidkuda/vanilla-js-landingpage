c0081b8d42c6ae0c89b07083a9713988c43c8244
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-07-19 22:17:13 +0200

    feat: now theme-selector for dark-mode or light-mode works

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const navbar = document.querySelector('#navbar__list');
 const mainElement = document.querySelector('main');
 const loading = document.querySelector('.loader');
 const addSectionButton = document.querySelector('.add-section-button');
+const bodyTag = document.querySelector('body');
+const buttonLightMode = document.querySelector('.button-light-mode')
+const buttonDarkMode = document.querySelector('.button-dark-mode')
 
 const loremIpsumString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
 const aliquamString = 'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.';
@@ -112,7 +115,16 @@ addSectionButton.addEventListener('click', function() {appendSection()});
 
 // toggle dark-mode / light-mode
 
-const toggleTheme = () => {
-    document.querySelector('body').classList.toggle();
+const activateDarkMode = () => {
+    bodyTag.className = 'dark-mode'
 };
+
+const activateLightMode = () => {
+    bodyTag.className = 'light-mode'
+};
+
+// event listener for dark-mode / light-mode
+
+buttonDarkMode.addEventListener('click', function() {activateDarkMode();})
+buttonLightMode.addEventListener('click', function () {activateLightMode();})
 
