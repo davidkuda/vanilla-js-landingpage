0c25d964df1ac2b226af80ba590e2ccbff4356bc
Ozzie Kuda <ozzie.kuda@gmail.com>
2020-07-23 23:41:17 +0200

    fix: now nav list element gets added only once

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ const buttonLightMode = document.querySelector('.button-light-mode');
 const buttonDarkMode = document.querySelector('.button-dark-mode');
 const activeSection = document.querySelector('.target');
 const allSections = document.getElementsByTagName('section');
+const initialSections = document.querySelectorAll('section');
 
 const loremIpsumString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
 const aliquamString = 'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.';
@@ -114,6 +115,22 @@ const activateLightMode = () => {
  * Begin Events
  * 
 */
+
+// dynamically populate nav
+
+window.addEventListener('scroll', function () {
+
+    for (section of initialSections) {
+        if (isInViewport(section)) {
+            if (document.getElementById(`navigation-section-${section.dataset.nav.slice(8,9)}`)) {
+                console.log('this is true')
+            } else {
+                const stringNewNavLiSpecific = `<a href="#section1"><li id="navigation-section-${section.dataset.nav.slice(8,9)}">${section.dataset.nav}</li></a>`;
+                navbar.insertAdjacentHTML('beforeend', stringNewNavLiSpecific);
+            }
+        }
+    }
+});
 
 // append a new section upon button click
 
